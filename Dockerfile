dfd552514348a636bfd5d6a327fa89983d61ad45
tomornw <dekcom007@gmail.com>
2020-06-20 19:26:57 +0700

    change

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,8 @@ LABEL maintainer="tomornw@mitrphol.com"
 
 COPY . /usr/local/tomcat/webapps/
 
+ENV TZ=Asia/Bangkok
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
 EXPOSE 8080
 CMD ["catalina.sh", "run"]
--- prod.sh
+++ prod.sh
@@ -5,12 +5,12 @@ echo "welcome"
 #
 echo "web_portal"
 echo "stop container and remove image"
-docker container stop web_portal
+docker container rm -f web_portal
 docker image rm -f web_portal
 echo "Go to Folder Script File And Dockerfile"
 cd ~/mitrphol/web_portal/
 echo "build image from docker file"
 docker build -t web_portal .
 echo "run image to container"
-docker run --rm --name web_portal -p 8080:8080 -d web_portal
+docker run --restart=always --name web_portal -p 8080:8080 -d web_portal
 echo "finish step"
